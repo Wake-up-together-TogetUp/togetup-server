1e45a961b4ba37e4e2ee5aa5cdf4d97ae03faff8
Lee ChanMi <anytime0224@gmail.com>
2023-04-18 07:15:12 +0900

    feature/file

--- src/main/java/com/wakeUpTogetUp/togetUp/config/WebSecurityConfig.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.wakeUpTogetUp.togetUp;
+package com.wakeUpTogetUp.togetUp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
