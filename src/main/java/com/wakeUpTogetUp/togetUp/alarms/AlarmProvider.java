331da71b21e1915e4b60d679cabe840efc6f859f
Lee ChanMi <anytime0224@gmail.com>
2023-05-09 23:06:08 +0900

    feature : Request Vallidation 추가
    - AlarmReq vallidation 추가
    - RoutineReq Vallidation 추가

--- src/main/java/com/wakeUpTogetUp/togetUp/alarms/AlarmProvider.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/alarms/AlarmProvider.java
@@ -8,6 +8,8 @@ import com.wakeUpTogetUp.togetUp.common.ResponseStatus;
 import com.wakeUpTogetUp.togetUp.routines.RoutineRepository;
 import com.wakeUpTogetUp.togetUp.routines.dto.response.RoutineRes;
 import com.wakeUpTogetUp.togetUp.routines.model.Routine;
+import com.wakeUpTogetUp.togetUp.users.UserRepository;
+import com.wakeUpTogetUp.togetUp.users.model.User;
 import com.wakeUpTogetUp.togetUp.utils.mappers.AlarmMapper;
 import com.wakeUpTogetUp.togetUp.utils.mappers.RoutineMapper;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,7 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class AlarmProvider {
+    private final UserRepository userRepository;
     private final AlarmRepository alarmRepository;
     private final RoutineRepository routineRepository;
 
@@ -28,10 +31,12 @@ public class AlarmProvider {
      * @return
      */
     public List<AlarmsRes> getAlarmsByUserId(Integer userId) {
-        List<Alarm> alarmList = alarmRepository.findByUserId(userId)
-                .orElseThrow(
-                        () -> new BaseException(ResponseStatus.ALARM_NOT_FOUND)
-                );
+        // 유저 id 유무 확인
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new BaseException(ResponseStatus.INVALID_USER_ID)
+        );
+
+        List<Alarm> alarmList = alarmRepository.findByUserId(userId);
 
         // dto 매핑
         ArrayList<AlarmsRes> alarmsResList = new ArrayList<>();
