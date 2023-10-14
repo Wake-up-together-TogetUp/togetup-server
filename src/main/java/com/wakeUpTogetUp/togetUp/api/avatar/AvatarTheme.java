4bf788d0e4cfc0e8b49729909614e96a18ce0bf9
Lee ChanMi <anytime0224@gmail.com>
2023-10-14 13:24:12 +0900

    #55 [FEAT] 아바타 서비스, 아바타 테마 ENUM 타입 생성

---
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/AvatarTheme.java
@@ -0,0 +1,16 @@
+package com.wakeUpTogetUp.togetUp.api.avatar;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum AvatarTheme {
+    NOOB_CHICKS("신입 병아리"),
+    TWINKLING_TEDDY_BEAR("눈을 반짝이는 곰돌이"),
+    CUTE_BUNNY("깜찍한 토끼")
+    ;
+
+    private final String value;
+}
