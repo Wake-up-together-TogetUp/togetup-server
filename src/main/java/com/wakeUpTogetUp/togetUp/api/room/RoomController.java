3c2eecdc98cb0c9e53f32ebe0d8f4a13f45df31c
hye-on <ain0103@naver.com>
2023-10-17 12:15:54 +0900

    #56 [DOCS] : 그룹디테일 스웨거 문서 수정

--- src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomController.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomController.java
@@ -102,7 +102,7 @@ public class RoomController {
 
     }
 
-    @Operation(summary = "그룹 디테일 보기",description = "그룹 디테일 보기 (설정 화면)")
+    @Operation(summary = "그룹 디테일 보기 and 초대 받은 사람에게 보이는 그룹 정보 보기 ",description = "그룹 디테일 보기 (설정 화면) and 초대 받은 사람에게 보이는 앱 내 페이지")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.",
                     content = @Content(schema = @Schema(implementation = RoomDetailRes.class))),
