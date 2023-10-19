5c7a342d7db28c0279ab842f7056502723ae1721
hye-on <ain0103@naver.com>
2023-10-19 20:00:30 +0900

    [FIX] : 그룹게시판 api 수정 - isAlarmActive(알람이 울리는 날 여부)를 클라이언트에서 받지 않고 서버에서 가져와서 response 반환하게 수정

--- src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/AvatarTheme.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/AvatarTheme.java
@@ -12,26 +12,9 @@ import java.util.Map;
 public enum AvatarTheme {
     NOOB_CHICK("신입 병아리"),
     TWINKLING_TEDDY_BEAR("눈을 반짝이는 곰돌이"),
-    CUTE_BUNNY("깜찍한 토끼")
-    ;
+    CUTE_BUNNY("깜찍한 토끼");
 
     private final String value;
 
-    private static final Map<String, AvatarTheme> valueToEnum = new HashMap<>();
 
-    // 열거형 상수 생성 시에 value를 영어로 매핑
-    static {
-        for (AvatarTheme theme : AvatarTheme.values()) {
-            valueToEnum.put(theme.value, theme);
-        }
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    // 한글 값을 받아서 해당 열거형 상수의 이름(영어)을 반환
-    public static AvatarTheme fromValue(String value) {
-        return valueToEnum.get(value);
-    }
 }
