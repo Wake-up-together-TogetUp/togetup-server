2db356d721681117af0512a3d4b25f115ae1db7a
Lee ChanMi <anytime0224@gmail.com>
2023-11-02 10:37:33 +0900

    [ENV] DB schema 수정사항 기록 모음 패키지 updates 추가
    - sql 파일 이름 컨벤션
    날짜[변경테이블1 변경테이블2 ...]

--- src/main/resources/migration/data.sql
+++ src/main/resources/migration/data.sql
@@ -1,4 +1,3 @@
-
 -- 미션
 INSERT INTO mission (id, name, created_at, is_active) VALUES (1, 'Authentication Shots', '2023-09-04 20:21:08', 1);
 INSERT INTO mission (id, name, created_at, is_active) VALUES (2, 'object recognition', '2023-08-25 16:51:30', 1);
@@ -82,52 +81,3 @@ INSERT INTO mission_object (id, name, kr, icon, is_active, mission_id) VALUES (7
 INSERT INTO togetup_local_cm.avatar (id, theme, price, unlock_level, created_at) VALUES (1, 'NOOB_CHICK', 0, 1, '2023-10-07 20:35:51');
 INSERT INTO togetup_local_cm.avatar (id, theme, price, unlock_level, created_at) VALUES (2, 'TWINKLING_TEDDY_BEAR', 1500, 15, '2023-10-21 03:24:06');
 INSERT INTO togetup_local_cm.avatar (id, theme, price, unlock_level, created_at) VALUES (3, 'CUTE_BUNNY', 1500, 30, '2023-10-21 03:24:06');
-
-
--- avatar에 unlock_level(해금 레벨) 컬럼 추가, phase 컬럼 삭제
-alter table avatar
-    add unlock_level int unsigned not null after price;
-
-alter table avatar
-drop column phase;
-
-
--- mission_log에 mission_id 삭제
-alter table mission_log
-    drop foreign key mission_log_ibfk_3;
-
-alter table mission_log
-    drop column mission_id;
-
--- user에 level, experience, point 컬럼 추가
-alter table user
-    add level int unsigned default 1 not null after agree_push;
-
-alter table user
-    add experience int unsigned default 0 not null after level;
-
-alter table user
-    add point int unsigned default 0 not null after experience;
-
-
-
--- alarm에 user_id, mission_id nullable 속성 수정
-alter table alarm
-    modify user_id int unsigned null;
-
-alter table alarm
-    modify mission_id int unsigned not null;
-
--- table : user_avatar_purchase_log 삭제
-DROP TABLE user_avatar_purchase_log;
-
--- table : user_avatar 현재 아바타 정보를 위한 컬럼 추가
-alter table user_avatar
-    add is_active tinyint(1) default 0 not null after id;
-
-alter table user_avatar
-    add created_at TIMESTAMP default CURRENT_TIMESTAMP not null after is_active;
-
--- table : avatar 이미지 링크 삭제
-alter table avatar
-drop column avatar_img_link;
