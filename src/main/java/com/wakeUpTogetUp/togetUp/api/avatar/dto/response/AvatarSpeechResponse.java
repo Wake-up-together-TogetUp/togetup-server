7e7734fdeb40fcda80c4cfc9d60deb698d6bbcad
Lee ChanMi <anytime0224@gmail.com>
2024-04-09 11:55:08 +0900

    #116 [FEAT] 아바타 대사 조건 ENUM 추가

--- src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/dto/response/AvatarSpeechResponse.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/dto/response/AvatarSpeechResponse.java
@@ -3,9 +3,11 @@ package com.wakeUpTogetUp.togetUp.api.avatar.dto.response;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class AvatarSpeechResponse {
 
     private String speech;
--- src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/model/AvatarSpeech.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/model/AvatarSpeech.java
@@ -2,6 +2,8 @@ package com.wakeUpTogetUp.togetUp.api.avatar.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class AvatarSpeech {
     @Column(columnDefinition = "VARCHAR(60)")
     private String speech;
 
+    @Enumerated(EnumType.STRING)
     @Column(columnDefinition = "VARCHAR(20)")
     private AvatarSpeechCondition condition;
 
