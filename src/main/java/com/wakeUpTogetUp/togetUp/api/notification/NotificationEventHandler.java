04b2077292ec8a482d94e92a5177783449c5c918
hye-on <ain0103@naver.com>
2024-04-18 21:40:41 +0900

    #106 [FIX] : 이벤트리스너를 이벤트핸들러로 이름 수정

--- src/main/java/com/wakeUpTogetUp/togetUp/api/notification/NotificationEventHandler.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/notification/NotificationEventHandler.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class NotificationEventListener {
+public class NotificationEventHandler {
     private final FcmService fcmService;
 
     @Async()
