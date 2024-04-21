cf0e29eb9cfcc6dd7e23bdfb2b674c9f3aabeb7b
hye-on <ain0103@naver.com>
2024-04-21 23:17:29 +0900

    #123 [FIX] : 도메인 변경 스웨거에 반영

--- src/main/java/com/wakeUpTogetUp/togetUp/config/OpenApiConfig.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/config/OpenApiConfig.java
@@ -20,7 +20,7 @@ class OpenApiConfig {
                 .description(Constant.API_DESCRIPTION);
 
         return new OpenAPI()
-                .addServersItem(new Server().url("https://togetup.shop"))
+                .addServersItem(new Server().url("https://togetup.site"))
                 .addServersItem(new Server().url("http://localhost:9010"))
                 .info(info)
                 .addSecurityItem(new SecurityRequirement().addList(Constant.BEARER))
