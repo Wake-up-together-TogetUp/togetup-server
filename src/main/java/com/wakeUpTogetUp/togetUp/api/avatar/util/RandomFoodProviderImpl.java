89fe101c2da71c10aa89b27d2df43e9c57b7ada9
Lee ChanMi <anytime0224@gmail.com>
2024-05-23 22:38:01 +0900

    [FEAT] RandomFoodProvider에서 반환하는 음식 종류 추가

--- src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/util/RandomFoodProviderImpl.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/avatar/util/RandomFoodProviderImpl.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class RandomFoodProviderImpl implements RandomFoodProvider {
     private static final List<String> FOODS = List.of(
-            "피자", "햄버거", "스시", "파스타", "샐러드", "김치찌개", "비빔밥", "초밥", "스테이크", "라면");
+            "피자", "햄버거", "스시", "파스타", "샐러드", "김치찌개", "비빔밥", "초밥", "스테이크",
+            "라면", "냉면", "갈비", "삼겹살", "떡볶이", "칼국수", "보쌈", "닭갈비", "감자탕", "카레", "돈까스",
+            "곱창", "치킨", "해물파전", "타코", "딤섬", "똠얌꿍", "팟타이", "케밥", "라멘", "우동", "타코야키",
+            "스키야키", "훠궈", "마라탕", "꿔바로우", "짜장면", "탕수육", "양꼬치", "깐풍기", "대나무");
 
     private static final Random RANDOM = new Random();
 
