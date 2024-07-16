f065bba3f8b23ea39633c56655c0eb3cfec444df
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-07-16 18:31:14 +0900

    [FEATURE] 그룹 인원 제한 락 구현 (#148)
    * [FEAT]: 그룹에 인원제한 lock 구현
    
    * [CHORE] : 사용하지 않는 코드 삭제

--- src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomRepository.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomRepository.java
@@ -3,11 +3,15 @@ package com.wakeUpTogetUp.togetUp.api.room;
 
 import com.wakeUpTogetUp.togetUp.api.room.dto.response.RoomDetailRes;
 import com.wakeUpTogetUp.togetUp.api.room.model.Room;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
 import java.util.Optional;
 
 @Repository
@@ -15,7 +19,12 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
 
     @Query("SELECT r FROM Room r JOIN Alarm a ON r.id = a.room.id WHERE a.id= :alarmId")
     Optional<Room> findByAlarmId(@Param("alarmId") Integer alarmId);
-    Optional<Room> findByInvitationCode(@Param("invitationCode") String invitationCode);
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints({
+            @QueryHint(name = "jakarta.persistence.lock.timeout", value = "10000")
+    })
+    @Query("SELECT r FROM Room r where r.id = :id ")
+    Optional<Room> findByIdUsingPessimisticLock(@Param("id") Integer id);
 
 }
--- src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomUserRepository.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomUserRepository.java
@@ -13,28 +13,10 @@ import java.util.Optional;
 @Repository
 public interface RoomUserRepository extends JpaRepository<RoomUser, Integer> {
 
-    List<RoomUser> findByUserId(Integer userId);
 
-    @Query("SELECT ru FROM RoomUser ru " +
-            "JOIN User u ON (ru.user.id = u.id) " +
-            "WHERE ru.room.id = :roomId " +
-            "ORDER BY " +
-            "CASE WHEN ru.user.id = :userId THEN 0 " +
-            "ELSE 1 " +
-            "END, u.name")
-
-    List<RoomUser> findAllByRoom_IdOrderByPreference(Integer roomId, Integer userId);
-
-
-    @Query("SELECT ru.room.id " +
-            "FROM RoomUser ru " +
-            "WHERE ru.user.id = :userId " +
-            "ORDER BY ru.createdAt DESC")
-    List<Integer> findAllRoomIdsByUserId(@Param("userId") Integer userId);
 
     Integer countByUserId(Integer userId);
 
-    Integer countByRoomId(Integer roomId);
 
     void deleteByUserId(Integer userId);
 
@@ -42,11 +24,8 @@ public interface RoomUserRepository extends JpaRepository<RoomUser, Integer> {
 
 
 
-    boolean existsRoomUserByRoom_IdAndUser_Id(Integer roomId, Integer userId);
 
-    void deleteByRoom_IdAndUser_Id(Integer roomId, Integer userId);
 
-    List<RoomUser> findAllByRoom_IdOrderByCreatedAt(Integer roomId);
 
 
 }
