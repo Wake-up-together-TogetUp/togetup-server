ce8782017751ae4ab1f1d440e810b2c7444a15b1
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-08-13 18:19:51 +0900

    [FIX]: roomlist 에서 똑같은 room 여러개 반환하는 에러 해결, missionObject여러개 반환 해 NonUniqueResultException나는 에러 해결 (#150)
    * [FIX]: roomlist 에서 똑같은 room 여러개 반환하는 에러 해결
    
    * [FIX]: missionObject여러개 반환 해 NonUniqueResultException나는 에러 해결

--- src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomQueryRepositoryImpl.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/room/RoomQueryRepositoryImpl.java
@@ -18,63 +18,64 @@ import java.util.Optional;
 @Repository
 public class RoomQueryRepositoryImpl implements RoomQueryRepository {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+	@PersistenceContext
+	private EntityManager entityManager;
 
-    @Override
-    public List<RoomRes> findRoomsOrderedByJoinTime(Integer userId) {
-        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
-        CriteriaQuery<RoomRes> cq = cb.createQuery(RoomRes.class);
+	@Override
+	public List<RoomRes> findRoomsOrderedByJoinTime(Integer userId) {
+		CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+		CriteriaQuery<RoomRes> cq = cb.createQuery(RoomRes.class);
 
-        Root<RoomUser> roomUserRoot = cq.from(RoomUser.class);
-        Join<RoomUser, Room> roomJoin = roomUserRoot.join("room");
-        Join<RoomUser, Alarm> alarmJoin = roomJoin.join("alarms");
-        Join<Alarm, MissionObject> missionObjectJoin = alarmJoin.join("missionObject");
+		Root<RoomUser> roomUserRoot = cq.from(RoomUser.class);
+		Join<RoomUser, Room> roomJoin = roomUserRoot.join("room");
+		Join<Room, Alarm> alarmJoin = roomJoin.join("alarms", JoinType.LEFT);
+		Join<Alarm, MissionObject> missionObjectJoin = alarmJoin.join("missionObject", JoinType.LEFT);
 
-        cq.select(cb.construct(
-                        RoomRes.class,
-                        roomJoin.get("id"),
-                        missionObjectJoin.get("icon"),
-                        roomJoin.get("name"),
-                        missionObjectJoin.get("name"),
-                        missionObjectJoin.get("kr")
-                )).where(cb.equal(roomUserRoot.get("user").get("id"), userId))
-                .orderBy(cb.desc(roomUserRoot.get("createdAt")));
+		cq.select(cb.construct(
+				RoomRes.class,
+				roomJoin.get("id"),
+				missionObjectJoin.get("icon"),
+				roomJoin.get("name"),
+				missionObjectJoin.get("name"),
+				missionObjectJoin.get("kr")
+			)).where(cb.equal(roomUserRoot.get("user").get("id"), userId))
+			.groupBy(roomJoin.get("id"))
+			.orderBy(cb.desc(roomUserRoot.get("createdAt")));
 
-        return entityManager.createQuery(cq).getResultList();
-    }
+		return entityManager.createQuery(cq).getResultList();
+	}
 
-    @Override
-    public Optional<Room> findById(Integer roomId) {
-        List<Room> rooms = entityManager.createQuery("SELECT r FROM Room r WHERE r.id = :roomId", Room.class)
-                .setParameter("roomId", roomId)
-                .getResultList();
+	@Override
+	public Optional<Room> findById(Integer roomId) {
+		List<Room> rooms = entityManager.createQuery("SELECT r FROM Room r WHERE r.id = :roomId", Room.class)
+			.setParameter("roomId", roomId)
+			.getResultList();
 
-        return rooms.stream().findFirst();
-    }
+		return rooms.stream().findFirst();
+	}
 
-    @Override
-    public List<RoomUser> findAllByRoomIdOrderByUserIdAndUserName(Integer userId, Integer roomId) {
-        return entityManager.createQuery(
-                        "SELECT ru " +
-                                "FROM RoomUser ru " +
-                                "WHERE ru.room.id = :roomId " +
-                                "ORDER BY CASE WHEN ru.user.id = :userId " +
-                                "THEN 0 ELSE 1 END, ru.user.name ASC ", RoomUser.class)
-                .setParameter("roomId", roomId)
-                .setParameter("userId", userId)
-                .getResultList();
-    }
+	@Override
+	public List<RoomUser> findAllByRoomIdOrderByUserIdAndUserName(Integer userId, Integer roomId) {
+		return entityManager.createQuery(
+				"SELECT ru " +
+					"FROM RoomUser ru " +
+					"WHERE ru.room.id = :roomId " +
+					"ORDER BY CASE WHEN ru.user.id = :userId " +
+					"THEN 0 ELSE 1 END, ru.user.name ASC ", RoomUser.class)
+			.setParameter("roomId", roomId)
+			.setParameter("userId", userId)
+			.getResultList();
+	}
 
-    @Override
-    public Optional<Room> findByInvitationCode(String invitationCode) {
-        List<Room> rooms = entityManager.createQuery("SELECT r FROM Room r WHERE r.invitationCode = :invitationCode", Room.class)
-                .setParameter("invitationCode", invitationCode)
-                .getResultList();
+	@Override
+	public Optional<Room> findByInvitationCode(String invitationCode) {
+		List<Room> rooms = entityManager.createQuery("SELECT r FROM Room r WHERE r.invitationCode = :invitationCode",
+				Room.class)
+			.setParameter("invitationCode", invitationCode)
+			.getResultList();
 
-        return rooms.stream().findFirst();
-    }
-
+		return rooms.stream().findFirst();
+	}
 
 }
 
