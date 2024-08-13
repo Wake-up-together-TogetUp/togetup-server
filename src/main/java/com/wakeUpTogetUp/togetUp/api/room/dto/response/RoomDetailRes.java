b60066745691702526f0a9dda66d3ac3181ad38c
조혜온 <68319395+hye-on@users.noreply.github.com>
2024-08-13 19:09:53 +0900

    [CHORE]: RoomDetailRes, RoomInviteInfoRes에 미션 영어이름 추가 (#153)
    * [CHORE]: RoomDetailRes, RoomInviteInfoRes에 미션 영어이름 추가
    
    * [CHORE] 띄어쓰기 오타 수정
    
    ---------
    
    Co-authored-by: 이찬미 <anytime0224@gmail.com>

--- src/main/java/com/wakeUpTogetUp/togetUp/api/room/dto/response/RoomDetailRes.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/room/dto/response/RoomDetailRes.java
@@ -4,62 +4,60 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
-
 
 @Schema(description = "룸정보 조회 요청 api 응답")
 @Getter
 @AllArgsConstructor(staticName = "of")
 public class RoomDetailRes {
 
-    @Schema(description = "룸 정보")
-    private RoomData roomData;
+	@Schema(description = "룸 정보")
+	private RoomData roomData;
 
-    @Schema(description = "미션 정보")
-    private MissionData missionData;
+	@Schema(description = "미션 정보")
+	private MissionData missionData;
 
-    @Schema(description = "룸의 유저들 정보 리스트")
-    private List<UserProfileData> userProfileData;
+	@Schema(description = "룸의 유저들 정보 리스트")
+	private List<UserProfileData> userProfileData;
 
-    @AllArgsConstructor(staticName = "of")
-    @Getter
-    public static class RoomData {
+	@AllArgsConstructor(staticName = "of")
+	@Getter
+	public static class RoomData {
 
+		@Schema(description = "room name", example = "설여대 기상방")
+		private String name;
 
+		@Schema(description = "room 소개", example = "소프트웨어융학학과 기상방입니다.")
+		private String intro;
 
-        @Schema(description = "room name", example = "설여대 기상방")
-        private String name;
+		@Schema(description = "room 개설일", example = "2020.02.03")
+		private LocalDate createdAt;
 
-        @Schema(description = "room 소개", example = "소프트웨어융학학과 기상방입니다.")
-        private String intro;
+		@Schema(description = "함께하는 인원수", example = "6")
+		private Integer headCount;
 
-        @Schema(description = "room 개설일", example = "2020.02.03")
-        private LocalDate createdAt;
+		@Schema(description = "룸 초대코드", example = "4c905")
+		private String invitationCode;
 
-        @Schema(description = "함께하는 인원수", example = "6")
-        private Integer headCount;
+	}
 
-        @Schema(description = "룸 초대코드", example = "4c905")
-        private String invitationCode;
+	@AllArgsConstructor(staticName = "of")
+	@Getter
+	public static class MissionData {
 
+		@Schema(description = "미션 아이콘", example = "⏰")
+		private String icon;
 
-    }
+		@Schema(description = "미션 이름(영어)", example = "car")
+		private String name;
 
-    @AllArgsConstructor(staticName = "of")
-    @Getter
-    public static class MissionData {
+		@Schema(description = "미션의 한국말", example = "자동차")
+		private String missionKr;
 
-
-        @Schema(description = "미션 아이콘", example = "⏰")
-        private String icon;
-
-        @Schema(description = "미션의 한국말", example = "자동차")
-        private String missionKr;
-
-    }
-
+	}
 
 }
--- src/main/java/com/wakeUpTogetUp/togetUp/api/room/dto/response/RoomInviteInfoRes.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/room/dto/response/RoomInviteInfoRes.java
@@ -11,38 +11,41 @@ import lombok.Setter;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 
-
 @Schema(description = "룸정보 조회 요청 api 응답")
 @Getter
 @AllArgsConstructor()
 public class RoomInviteInfoRes {
 
+	@Schema(description = "룸아이디", example = "1")
+	private Integer id;
 
-    @Schema(description = "룸아이디", example = "1")
-    private Integer id;
+	@Schema(description = "미션 아이콘", example = "⏰")
+	private String icon;
 
-    @Schema(description = "미션 아이콘", example = "⏰")
-    private String icon;
+	@Schema(description = "room name", example = "설여대 기상방")
+	private String name;
 
-    @Schema(description = "room name", example = "설여대 기상방")
-    private String name;
+	@Schema(description = "room 소개", example = "소프트웨어융학학과 기상방입니다.")
+	private String intro;
 
-    @Schema(description = "room 소개", example = "소프트웨어융학학과 기상방입니다.")
-    private String intro;
+	@Schema(description = "room 개설일", example = "2020.02.03")
+	private LocalDate createdAt;
 
-    @Schema(description = "room 개설일", example = "2020.02.03")
-    private LocalDate createdAt;
+	@Schema(description = "함께하는 인원수", example = "6")
+	private Integer headCount;
 
-    @Schema(description = "함께하는 인원수", example = "6")
-    private Integer headCount;
+	@Schema(description = "미션 객체 id", example = "1")
+	private Integer missionObjectId;
 
-    @Schema(description = "미션 객체 id", example = "1")
-    private Integer missionObjectId;
+	@Schema(description = "미션의 한국말", example = "자동차")
+	private String missionKr;
 
-    @Schema(description = "미션의 한국말", example = "자동차")
-    private String missionKr;
+	@Schema(description = "미션 이름(영어)", example = "car")
+	private String missionName;
 
-    public static RoomInviteInfoRes of(Integer id, String icon, String name, String intro, Timestamp createdAt, Integer headCount, Integer missionObjectId, String missionKr) {
-        return new RoomInviteInfoRes(id, icon, name, intro, createdAt.toLocalDateTime().toLocalDate(), headCount, missionObjectId, missionKr);
-    }
+	public static RoomInviteInfoRes of(Integer id, String icon, String name, String intro, Timestamp createdAt,
+		Integer headCount, Integer missionObjectId, String missionKr, String missionName) {
+		return new RoomInviteInfoRes(id, icon, name, intro, createdAt.toLocalDateTime().toLocalDate(), headCount,
+			missionObjectId, missionKr, missionName);
+	}
 }
