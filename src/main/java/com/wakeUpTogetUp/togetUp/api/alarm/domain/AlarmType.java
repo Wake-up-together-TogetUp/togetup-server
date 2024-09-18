73a08b616ed3eb5f6c9d555eb3ba51cd2c8f0021
이찬미 <anytime0224@gmail.com>
2024-09-18 15:31:37 +0900

    [FIX] 브리핑 보드 알람 타임라인 정렬 에러 해결 (#168)
    * [FIX] 쿼리 조건 간단화 및 cross join 방지
    
    * [FIX] cross join 방지 및 미션 기록 생성일자로 정렬 순서 변경
    
    * #133 [refactor, feat] 개인/그룹 알람 목록 가져오기 쿼리를 동적으로 생성 및 활성화 알람을 상단으로 표시하기
    
    * #133 [feat] 알람 목록 쿼리 조건 메서드 분리 및 alarm type 에러 처리 세분화
    
    * #133 [refactor] 필요없는 주석 제거
    
    * #133 [FIX] 알람 활성화 여부에 따른 정렬 삭제

--- src/main/java/com/wakeUpTogetUp/togetUp/api/alarm/domain/AlarmType.java
+++ src/main/java/com/wakeUpTogetUp/togetUp/api/alarm/domain/AlarmType.java
@@ -1,6 +1,19 @@
 package com.wakeUpTogetUp.togetUp.api.alarm.domain;
 
+import com.wakeUpTogetUp.togetUp.common.Status;
+import com.wakeUpTogetUp.togetUp.exception.BaseException;
+import java.util.Arrays;
+
 public enum AlarmType {
     PERSONAL,
     GROUP;
+
+    public static AlarmType getByName(String name) {
+        return Arrays.stream(values())
+                .filter(value -> value.name().equals(name))
+                .findFirst()
+                .orElseThrow(
+                        () -> new BaseException(Status.ALARM_TYPE_NOT_FOUND)
+                );
+    }
 }
